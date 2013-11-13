670e5372de0cda64caf0856551a354b626559054
equeim <equeim@gmail.com>
2013-11-13 22:47:25 +0400

    russian translation

---
+++ Sailfish/l10n/ru_RU.ts
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../pages/AboutPage.qml" line="32"/>
+        <source>About ownNotes</source>
+        <translation>О ownNotes</translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <location filename="../pages/EditPage.qml" line="93"/>
+        <source>Publish to Scriptogr.am</source>
+        <translation>Опубликовать в Scriptogr.am</translation>
+    </message>
+    <message>
+        <location filename="../pages/EditPage.qml" line="99"/>
+        <source>Publish as Post to KhtCms</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pages/EditPage.qml" line="105"/>
+        <source>Publish as Page to KhtCms</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../pages/MainPage.qml" line="58"/>
+        <source>Deleting</source>
+        <translation>Удаление</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="104"/>
+        <source>Category</source>
+        <translation>Категория</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="110"/>
+        <source>Duplicate</source>
+        <translation>Сделать копию</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="116"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="150"/>
+        <location filename="../pages/MainPage.qml" line="151"/>
+        <location filename="../pages/MainPage.qml" line="195"/>
+        <source>Sync</source>
+        <translation>Синхронизировать</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="157"/>
+        <source>Search</source>
+        <translation>Поиск</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="179"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="185"/>
+        <source>Notes in your own cloud !</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="191"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="203"/>
+        <location filename="../pages/MainPage.qml" line="216"/>
+        <source>New note</source>
+        <translation>Новая заметка</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="229"/>
+        <source>No notes.</source>
+        <translation>Нет заметок.</translation>
+    </message>
+    <message>
+        <location filename="../pages/MainPage.qml" line="304"/>
+        <source>Category:</source>
+        <translation>Категория:</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="23"/>
+        <source>ownNotes Settings</source>
+        <translation>Настройки ownNotes</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="27"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="34"/>
+        <source>Font:</source>
+        <translation>Шрифт:</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="75"/>
+        <source>Size</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="93"/>
+        <source>ownCloud/WebDav Sync</source>
+        <translation>Синхр. с ownCloud/WebDav</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="98"/>
+        <location filename="../pages/SettingsPage.qml" line="100"/>
+        <location filename="../pages/SettingsPage.qml" line="167"/>
+        <location filename="../pages/SettingsPage.qml" line="169"/>
+        <source>Url</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="108"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="118"/>
+        <location filename="../pages/SettingsPage.qml" line="120"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="129"/>
+        <location filename="../pages/SettingsPage.qml" line="131"/>
+        <source>Remote Folder Name</source>
+        <translation>Имя удаленного каталога</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="138"/>
+        <source>Use auto merge</source>
+        <translation>Использовать авто объединение</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="139"/>
+        <source>When note are edited in several places, ownNotes will try to merge the changes if possible</source>
+        <translation>Если запись редактируется в нескольких местах, ownNotes будет пытаться объединить изменения</translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="148"/>
+        <source>Scriptogr.am</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="153"/>
+        <location filename="../pages/SettingsPage.qml" line="155"/>
+        <source>User ID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="162"/>
+        <source>KhtCms</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="177"/>
+        <location filename="../pages/SettingsPage.qml" line="179"/>
+        <source>Api Key</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
